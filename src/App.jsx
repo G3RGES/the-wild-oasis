3bc0faa0073a8546e33ca56b3e1fb638b1886984
G3RGES <georgios.nashaat@gmail.com>
2026-02-23 05:41:25 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import Button from "./ui/Button";
 import Input from "./ui/Input";
 import Heading from "./ui/Heading";
 import Row from "./ui/Row";
+import Form from "./ui/Form";
+import ButtonGroup from "./ui/ButtonGroup";
 
 const StyledApp = styled.div`
-  background-color: orangered;
+  /* background-color: orangered; */
   padding: 20px;
 `;
 
@@ -15,20 +17,28 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Row type="horizontal">
-          <Heading as="h1">Wild Oasis</Heading>
-          <div>
-            <Heading as="h1">Check in/out</Heading>
-            <Button>Check in</Button>
-            <Button>Check out</Button>
-          </div>
-        </Row>
-        <Row type="vertical">
-          <Heading as="h2">Guests </Heading>
-          <form>
-            <Input type="number" placeholder="Guests" />
-            <Input type="number" placeholder="Guests" />
-          </form>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">Wild Oasis</Heading>
+            <div>
+              <Heading as="h1">Check in/out</Heading>
+              <ButtonGroup>
+                <Button variation="primary" size="medium">
+                  Check in
+                </Button>
+                <Button variation="secondary" size="small">
+                  Check out
+                </Button>
+              </ButtonGroup>
+            </div>
+          </Row>
+          <Row>
+            <Heading as="h2">Guests </Heading>
+            <Form>
+              <Input type="number" placeholder="Guests" />
+              <Input type="number" placeholder="Guests" />
+            </Form>
+          </Row>
         </Row>
       </StyledApp>
     </>
