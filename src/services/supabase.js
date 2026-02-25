607b5abcc12cfc6cba7160390ee7190fa0ccee75
G3RGES <georgios.nashaat@gmail.com>
2026-02-25 11:09:44 +0200

    Create supabase.js

---
+++ src/services/supabase.js
@@ -0,0 +1,10 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://rnfhyxtxtbsuxjhirwrp.supabase.co";
+// const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseKey = "sb_publishable_8Tg07FXSN26zYUb76t5cXQ_EbzvYsgR";
+// const supabaseKey =
+//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJuZmh5eHR4dGJzdXhqaGlyd3JwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE5NjYwMjcsImV4cCI6MjA4NzU0MjAyN30.2jJFAIeGpKLbmnDhIqTfX8uUJUotZOjKh6ugQMvyxtQ";
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
