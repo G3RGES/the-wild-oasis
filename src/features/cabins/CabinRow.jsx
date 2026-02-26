0096758d105efb859034356d4bc5fcc318a76811
G3RGES <georgios.nashaat@gmail.com>
2026-02-26 05:56:07 +0200

    Update CabinRow.jsx

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,4 +1,6 @@
+/* eslint-disable react/prop-types */
 import styled from "styled-components";
+import { formatCurrency } from "../../utils/helpers";
 
 const TableRow = styled.div`
   display: grid;
@@ -39,8 +41,19 @@ const Discount = styled.div`
   color: var(--color-green-700);
 `;
 
-function CabinRow() {
-  return <div></div>;
+function CabinRow({ cabin }) {
+  const { name, maxCapacity, regularPrice, discount, image } = cabin;
+
+  return (
+    <TableRow role="row">
+      <Img src={image} alt={name} />
+      <Cabin>{name}</Cabin>
+      <div>Fits up to {maxCapacity} guests </div>
+      <Price>{formatCurrency(regularPrice)}</Price>
+      <Discount>{formatCurrency(discount)}</Discount>
+      <button>Delete</button>
+    </TableRow>
+  );
 }
 
 export default CabinRow;
