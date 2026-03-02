f6af4f375f66c9bec5fbe5019485ea98db9765ac
G3RGES <georgios.nashaat@gmail.com>
2026-03-02 15:51:21 +0200

    Create useUpdateCabin.js

---
+++ src/features/cabins/useUpdateCabin.js
@@ -0,0 +1,22 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { createEditCabin } from "../../services/apiCabins";
+
+export function useUpdateCabin() {
+  const queryClient = useQueryClient();
+
+  const { mutate: editCabin, isLoading: isEditting } = useMutation({
+    mutationFn: ({ newCabinData, id }) => createEditCabin(newCabinData, id),
+    onSuccess: () => {
+      toast.success("Cabin successfully updated");
+      queryClient.invalidateQueries({ queryKey: ["cabins"] });
+      //   reset();
+      //   setShowForm(false);
+    },
+    onError: (err) => {
+      toast.error("Cabin could not be created", err.message);
+    },
+  });
+
+  return { isEditting, editCabin };
+}
