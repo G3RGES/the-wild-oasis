0f1e7e5769f83d7e8c9276f6e52971583449a4f5
G3RGES <georgios.nashaat@gmail.com>
2026-03-02 16:29:06 +0200

    Update UpdateSettingsForm.jsx

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -17,6 +17,14 @@ function UpdateSettingsForm() {
     } = {},
   } = useSettings();
 
+  function handleUpdate(e, field) {
+    const { value } = e.target;
+
+    if (!value) return;
+
+    editSettings({ [field]: value });
+  }
+
   if (isLoading) return <Spinner />;
 
   return (
@@ -26,30 +34,37 @@ function UpdateSettingsForm() {
           type="number"
           id="min-nights"
           defaultValue={minBookingLength}
+          onBlur={(e) => handleUpdate(e, "minBookingLength")}
           disabled={isUpdating}
         />
       </FormRow>
+
       <FormRow label="Maximum nights/booking">
         <Input
           type="number"
           id="max-nights"
           defaultValue={maxBookingLength}
+          onBlur={(e) => handleUpdate(e, "maxBookingLength")}
           disabled={isUpdating}
         />
       </FormRow>
+
       <FormRow label="Maximum guests/booking">
         <Input
           type="number"
           id="max-guests"
           defaultValue={maxGuestsPerBooking}
+          onBlur={(e) => handleUpdate(e, "maxGuestsPerBooking")}
           disabled={isUpdating}
         />
       </FormRow>
+
       <FormRow label="Breakfast price">
         <Input
           type="number"
           id="breakfast-price"
           defaultValue={breakfastPrice}
+          onBlur={(e) => handleUpdate(e, "breakfastPrice")}
           disabled={isUpdating}
         />
       </FormRow>
