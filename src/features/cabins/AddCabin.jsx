934b075ca0b467fb3dd7d7681e6ca226c5f5d12f
G3RGES <georgios.nashaat@gmail.com>
2026-03-04 12:44:08 +0200

    Update AddCabin.jsx

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,17 +1,22 @@
 import { useState } from "react";
 import Button from "../../ui/Button";
 import CreateCabinForm from "./CreateCabinForm";
+import Modal from "../../ui/Modal";
 
 function AddCabin() {
-  const [showForm, setShowForm] = useState(false);
+  const [isOpenModal, setIsOpenModal] = useState(false);
 
   return (
     <div>
-      <Button onClick={() => setShowForm((show) => !show)}>
+      <Button onClick={() => setIsOpenModal((show) => !show)}>
         Add New Cabin
       </Button>
 
-      {showForm && <CreateCabinForm setShowForm={setShowForm} />}
+      {isOpenModal && (
+        <Modal onClose={() => setIsOpenModal(false)}>
+          <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
+        </Modal>
+      )}
     </div>
   );
 }
