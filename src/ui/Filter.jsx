22648287aa1e6452cf473a371b84b6c839116ec2
G3RGES <georgios.nashaat@gmail.com>
2026-03-08 14:51:25 +0200

    Update Filter.jsx

--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -17,7 +17,7 @@ const FilterButton = styled.button`
   border: none;
 
   ${(props) =>
-    props.active &&
+    props.$active &&
     css`
       background-color: var(--color-brand-600);
       color: var(--color-brand-50);
@@ -38,10 +38,11 @@ const FilterButton = styled.button`
 
 function Filter({ filterField, options }) {
   const [searchParams, setSearchParams] = useSearchParams();
-  const currentFilter = searchParams.get(filterField) || options[0].value;
+  const currentFilter = searchParams.get(filterField) || options.at(0).value;
 
   function handleClick(value) {
     searchParams.set(filterField, value);
+    if (searchParams.get("page")) searchParams.set("page", 1);
 
     setSearchParams(searchParams);
   }
@@ -50,10 +51,10 @@ function Filter({ filterField, options }) {
     <StyledFilter>
       {options.map((option) => (
         <FilterButton
-          active={option.value === currentFilter}
-          disabled={option.value === currentFilter}
           key={option.value}
           onClick={() => handleClick(option.value)}
+          $active={option.value === currentFilter}
+          disabled={option.value === currentFilter}
         >
           {option.label}
         </FilterButton>
