18b08defc1c0abdd83ad6d7e8bda2be3a099af30
G3RGES <georgios.nashaat@gmail.com>
2026-03-08 13:58:45 +0200

    Update BookingTable.jsx

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -7,7 +7,7 @@ import Spinner from "./../../ui/Spinner";
 import Pagination from "../../ui/Pagination";
 
 function BookingTable() {
-  const { bookings, isLoading: isLoadingBookings } = useBookings();
+  const { bookings, isLoading: isLoadingBookings, count } = useBookings();
 
   if (isLoadingBookings) return <Spinner />;
 
@@ -33,7 +33,7 @@ function BookingTable() {
         />
 
         <Table.Footer>
-          <Pagination count={15} />
+          <Pagination count={count} />
         </Table.Footer>
       </Table>
     </Menus>
