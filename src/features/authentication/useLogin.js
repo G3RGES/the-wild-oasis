3994d34bf7cfdbacc5f8617e3c26889dd3f2536d
G3RGES <georgios.nashaat@gmail.com>
2026-03-10 16:32:35 +0200

    Update useLogin.js

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -8,9 +8,9 @@ export function useLogin() {
 
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
-    onSuccess: (user) => {
+    onSuccess: () => {
       toast.success("Logged in successfully");
-      console.log(user);
+
       navigate("/dashboard");
     },
     onError: (err) => {
