15ebe12eddc02a2c344fe9ff3023e797f74a71ab
G3RGES <georgios.nashaat@gmail.com>
2026-03-10 17:54:09 +0200

    Create useLogout.js

---
+++ src/features/authentication/useLogout.js
@@ -0,0 +1,19 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { logout as logoutApi } from "../../services/apiAuth";
+import { useNavigate } from "react-router-dom";
+
+export function useLogout() {
+  const navigate = useNavigate();
+  const queryClitent = useQueryClient();
+
+  const { mutate: logout, isLoading } = useMutation({
+    mutationFn: logoutApi,
+    onSuccess: () => {
+      //* REMOVE ALL QUERIES FROM CACHE
+      queryClitent.removeQueries();
+      navigate("/login", { replace: true });
+    },
+  });
+
+  return { logout, isLoading };
+}
