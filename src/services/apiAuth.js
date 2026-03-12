b80e6982a85b5bdd91d037d2be1ce21dc91f5cbd
G3RGES <georgios.nashaat@gmail.com>
2026-03-12 06:12:27 +0200

    Update apiAuth.js

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function signup({ fullName, email, password }) {
   const { data, error } = await supabase.auth.signUp({
@@ -47,4 +47,34 @@ export async function getCurrentUser() {
 export async function logout() {
   const { error } = await supabase.auth.signOut();
   if (error) throw new Error(error.message);
+}
+
+export async function updateCurrentUser({ password, fullName, avatar }) {
+  //*1. UPDATE PASSWORD OR FULLNAME
+  let updateData;
+
+  if (password) updateData = { password };
+  if (fullName) updateData = { data: { fullName } };
+
+  const { data, error } = await supabase.auth.updateUser(updateData);
+  if (error) throw new Error(error.message);
+  if (!avatar) return data;
+
+  //*2. UPDATE AVATAR IMAGE
+  const fileName = `avatar=${data.user.id}-${Math.random()}`;
+  const { error: storageError } = await supabase.storage
+    .from("avatars")
+    .upload(fileName, avatar);
+  if (storageError) throw new Error(error.message);
+
+  //*3. UPDATE AVATAR IN USER
+  const { data: updatedUser, error: userError } =
+    await supabase.auth.updateUser({
+      data: {
+        avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
+      },
+    });
+
+  if (userError) throw new Error(userError.message);
+  return updatedUser;
 }
