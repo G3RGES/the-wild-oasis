110803ee565dc59ed034a601dcd7e8e62b8433db
G3RGES <georgios.nashaat@gmail.com>
2026-03-12 08:00:00 +0200

    Create DarkModeContext.jsx

---
+++ src/context/DarkModeContext.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable react/prop-types */
+import { createContext, useContext, useEffect } from "react";
+import { useLocalStorageState } from "./../hooks/useLocalStorageState";
+
+const DarkModeContext = createContext();
+
+function DarkModeProvider({ children }) {
+  const [isDarkMode, setIsDarkMode] = useLocalStorageState(false, "darkMode");
+
+  useEffect(
+    function () {
+      if (isDarkMode) document.documentElement.classList.add("dark-mode");
+      else document.documentElement.classList.remove("dark-mode");
+    },
+    [isDarkMode],
+  );
+
+  function toggleDarkMode() {
+    setIsDarkMode((isDark) => !isDark);
+  }
+
+  return (
+    <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+}
+
+function useDarkMode() {
+  const context = useContext(DarkModeContext);
+
+  if (context === undefined) {
+    throw new Error("useDarkMode must be used within a DarkModeToggle");
+  }
+
+  return context;
+}
+
+export { DarkModeProvider, useDarkMode };
